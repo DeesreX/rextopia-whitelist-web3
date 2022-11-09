6e3d02a21059249dcd06d8583b6f68122a7f191f
DeesreX <deesrex@gmail.com>
2022-11-09 03:46:04 -0800

    New index

--- pages/index.js
+++ pages/index.js
@@ -9,36 +9,8 @@ import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <Link href="/whitelist">
-          <button className={styles.button}>Whitelist</button>
-        </Link>
-        <Link href="/nfts">
-          <button className={styles.button}>NFTs</button>
-        </Link>
-        <Link href="/rextoken">
-          <button className={styles.button}>RexToken</button>
-        </Link>
-        <Link href="/dao">
-          <button className={styles.button}>DAO</button>
-        </Link>
-        <Link href="/exchange">
-          <button className={styles.button}>Exchange</button>
-        </Link>
-        <Link href="/ens">
-          <button className={styles.button}>ENS</button>
-        </Link>
-        <Link href="/rextopia">
-          <button className={styles.button}>RextopiA</button>
-        </Link>
-        <Link href="/ceramic">
-          <button className={styles.button}>Ceramic</button>
-        </Link>
-        <Link href="/rgw">
-          <button className={styles.button}>Random Game Winner</button>
-        </Link>
-      </main>
+    <div>
+      <h1>Algebra Calculator</h1>
     </div>
   );
 }
